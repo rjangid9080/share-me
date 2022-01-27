da9e589aa880690002be4d8e0bf995f2a362d4c0
rjangid9080 <grut.rjangid9080@gmail.com>
2022-01-27 23:33:51 +0530

    adding-features

--- frontend/src/container/Home.jsx
+++ frontend/src/container/Home.jsx
@@ -1,7 +1,32 @@
-import React from 'react';
+import React, { useState, useRef, useEffect } from "react";
+import { Link, Route, Routes } from "react-router-dom";
+import { AiFillCloseCircle } from "react-icons/ai";
+import { HiMenuAlt1 } from "react-icons/hi";
+
+import { Sidebar, UserProfile } from "../components";
+import Pins from "./Pins";
+import logo from "../assets/logo.png";
+import { client } from "../client";
 
 const Home = () => {
-  return <div>Home</div>;
+  const [ toggleSidebar , setToggleSidebar] = useState(false);
+  return (
+    <div className="flex bg-gray-50 md:flex-row flex-col h-screen transaction-height duration-75 ease-out">
+      <div className="hidden md:flex h-screen flex-initial">
+        <Sidebar />
+      </div>
+      <div className="flex md:hidden flex-row">
+        <HiMenuAlt1
+          fontSize={40}
+          className="cursor-pointer"
+          onClick={() => setToggleSidebar(false)}
+        />
+        <Link to="/">
+          <img src={logo} alt="logo" className="w-28" />
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
