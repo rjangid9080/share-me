85f2b4860a08322e895b1a83af977b61f431cdfc
rjangid9080 <grut.rjangid9080@gmail.com>
2022-02-01 22:30:00 +0530

    creating-user-profile

--- frontend/src/components/UserProfile.jsx
+++ frontend/src/components/UserProfile.jsx
@@ -1,9 +1,38 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
+import { AiOutlineLogout } from "react-icons/ai";
+import { useParams, useNavigate } from "react-router-dom";
+import { GoogleLogout } from "react-google-login";
+
+import {
+  userCreatedPinsQuery,
+  userQuery,
+  userSavedPinsQuery,
+} from "../utils/data";
+import { client } from "../client";
+import MasonryLayout from "./MasonryLayout";
+import Spinner from "./Spinner";
 
 const UserProfile = () => {
-  return (<div>
-      UserProfile
-  </div>);
+  const [user, setUser] = useState(null);
+  const [pins, setPins] = useState(null);
+  const [text, setText] = useState("Created");
+  const [activeBtn, setActiveBtn] = useState("Created");
+  const navigate = useNavigate();
+  const { userId } = useParams();
+
+  useEffect(() => {
+    const query = userQuery(userId);
+
+    client.fetch(query).then(
+      (data) => {
+        setUser(data[0]);
+      },
+      [userId]
+    );
+  }, [userId]);
+  if (!user) return <Spinner message="Loading profile..." />;
+
+  return <div>UserProfile</div>;
 };
 
 export default UserProfile;
